a1583400ac35205c29f54fc277e4abe3cf7cdeec
TedChen566 <tedchen566@gmail.com>
2018-04-16 00:56:37 -0400

    essential cpp CH3 learnning code practice~~~

--- EssentialCPP/CH3.cc
+++ EssentialCPP/CH3.cc
@@ -130,6 +130,28 @@ IteratorType find( IteratorType first, IteratorType last,
 	return last;
 }
 
+const int asize = 8;
+int ia[ asize ] = { 1, 1, 2, 3, 5, 8, 13, 21 };
+
+vector<int> ivec( ia, ia+asize );
+list<int> ilist( ia, ia+asize );
+
+int *pia = find( ia, ia+asize, 1024 );
+if ( pia != ia + asize )
+	
+vector<int>::iterator it;
+it = find( ivec.begin(), ivec.end(), 1024 );
+if ( it !=ivec.end() )
+	
+
+list<int>::iterator iter;
+iter = find( ilist.begin(), ilist.end(), 1024);
+if ( iter != ilist.end() )
+	
+
+
+	
+
 
 				
 
