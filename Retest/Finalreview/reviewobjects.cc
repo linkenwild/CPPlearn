5d734f0bb386c85ac0d99d9fee8d378a71d23b75
TedChen566 <tedchen566@gmail.com>
2018-05-05 23:50:06 -0400

    finalreview

---
+++ Retest/Finalreview/reviewobjects.cc
@@ -0,0 +1,26 @@
+//final review
+//review objects
+#include <iostream>
+using namespace std;
+
+class A{
+	virtual void f() = 0;
+	virtual void g() = 0;
+};
+
+class B : 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
